980fd45c790a7da8b8ffb66ac2fe1dfbdf8a5c71
nicosp <nicolassanpedro@gmail.com>
2017-10-26 22:54:43 -0300

    Init

---
+++ test/first_spec.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+describe("A feature", function(){
+	describe("A Scenario", function(){
+	it("does something expected", function(){
+	assert(true);		
+    });	
+  });
+});
